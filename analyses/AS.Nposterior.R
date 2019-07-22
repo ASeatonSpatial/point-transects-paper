563dc1d8198bf1764e5bb57968b52f31b7d0ea79
A Seaton <andy.e.seaton@gmail.com>
2019-07-22 17:17:49 +0100

    fix n.samples

--- analyses/AS.Nposterior.R
+++ analyses/AS.Nposterior.R
@@ -12,14 +12,15 @@ AS.Nposterior <- function(model, predpts, ...){
   Lambda <- predict(model, predpts, ~ sum(weight * exp(grf + Intercept))) 
   Lambda
   
-  Nlow <- round(Lambda$mean - 4*Lambda$sd)
-  Nhigh <- round(Lambda$mean + 4*Lambda$sd)
+  Nlow <- round(Lambda$mean - 3*Lambda$sd)
+  Nhigh <- round(Lambda$mean + 3*Lambda$sd)
   by <- round((Nhigh - Nlow)/500)   # should give roughly 500 
   
   Nrange <- seq(Nlow, Nhigh, by = by)
   Npost = predict(model, predpts, 
                  ~ data.frame(N = Nrange, 
-                              dpois(Nrange, lambda = sum(weight * exp(grf + Intercept)))))
+                              dpois(Nrange, lambda = sum(weight * exp(grf + Intercept)))),
+                 ...)
   
   return(list(Npost = Npost, Nexp = Lambda))
 }

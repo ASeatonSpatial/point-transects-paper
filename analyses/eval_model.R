a996472f0959b5f5ade4e0dc8c89c19e345dc7e5
A Seaton <andy.e.seaton@gmail.com>
2019-08-01 10:34:37 +0100

    set plot dimensions

--- analyses/eval_model.R
+++ analyses/eval_model.R
@@ -2,6 +2,7 @@
 library(INLA)
 library(inlabru)
 
+set.seed(9701071)
 
 #### WARNING - as it stands this script will overwrite figures for the paper
 
@@ -36,18 +37,21 @@ summary(fit)
 # half-normal
 
 W = 60   # transect radius
+distdf <- data.frame(distance = seq(1,W,length=100))
 
 hnpred <- predict(fit, distdf, ~hn(distance,lsig))
 ggplot() + 
-  gg(hnpred)
+  gg(hnpred) +
+  ggtitle("Posterior half-normal detection function")
 
-ggsave(filename = "../figures/halfnormal.png")
+ggsave(filename = "../figures/halfnormal.png",
+       width = 5, height = 5, units = "in")
 
-# half-normal polar coords
-distdf <- data.frame(distance = seq(1,W,length=100))
-dfun <- predict(fit, distdf, ~ dsamp(distance,lsig))
-ggplot() + 
-  gg(dfun)
+# half-normal polar coords?  Is that what this is?  
+# distdf <- data.frame(distance = seq(1,W,length=100))
+# dfun <- predict(fit, distdf, ~ dsamp(distance,lsig))
+# ggplot() + 
+#   gg(dfun)
 
 # intensity
 pxl = pixels(mesh, nx = 100, ny = 100, mask = study_area)
@@ -56,41 +60,131 @@ pr.int <- predict(fit, pxl, ~ exp(grf + Intercept))
 ggplot() + 
   gg(study_area) + 
   gg(pr.int) +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c() +
+  ggtitle("Posterior mean intensity field")
 
-ggsave(filename = "../figures/intensity_mean.png")
+ggsave(filename = "../figures/intensity_mean.png",
+       width = 5, height = 6, units = "in")
 
 ggplot() +
   gg(study_area)  +
   gg(pr.int["cv"]) +
   gg(samplers, colour = "red") +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c() +
+  ggtitle("Posterior CV of intensity field")
 
-ggsave(filename = "../figures/intensity_cv.png")
+ggsave(filename = "../figures/intensity_cv.png",
+       width = 5, height = 6, units = "in")
+
+# lower and upper quantiles
+
+v = c(pr.int["mean"]@data,
+      pr.int["q0.025"]@data,
+      pr.int["q0.975"]@data)
+
+viridisscale =   scale_fill_viridis_c(limits = range(v))
+
+p1 = ggplot() + 
+  gg(study_area) + 
+  gg(pr.int) +
+  viridisscale
+
+p2 = ggplot() +
+  gg(study_area) +
+  gg(pr.int["q0.025"]) +
+  viridisscale
+
+p3 = ggplot() +
+  gg(study_area) +
+  gg(pr.int["q0.975"]) +
+  viridisscale
+
+layout_matrix <- matrix(c(4, 1, 1, 4, 2, 2, 3, 3), nrow = 2, byrow = TRUE)  # triangle arrangement
+
+png(filename = "../figures/intensity_quantiles.png",
+    width = 10, height = 10, units = "in", res = 100)
+multiplot(p1, p2, p3, layout = layout_matrix, byrow = TRUE, ncol = 2)
+dev.off()
+
+# show that these maps are misleading:
+str(pr.int)
+cell_area = as.numeric(pr.int@grid@cellsize["x"]*pr.int@grid@cellsize["y"])
+sum(pr.int@data["mean"]*cell_area)
+sum(pr.int@data["q0.025"]*cell_area)
+sum(pr.int@data["q0.975"]*cell_area)
+
+# plot "realized intensity" a few times
+draw1 = predict(fit, pxl, ~ exp(grf + Intercept), n.samples = 1)
+draw2 = predict(fit, pxl, ~ exp(grf + Intercept), n.samples = 1)
+draw3 = predict(fit, pxl, ~ exp(grf + Intercept), n.samples = 1)
+
+draw_all = c(draw1["mean"]@data,
+      draw2["mean"]@data,
+      draw3["mean"]@data)
+
+viridisscale =   scale_fill_viridis_c(limits = range(draw_all))
+
+p1 = ggplot() + 
+  gg(study_area) + 
+  gg(draw1["mean"]) +
+  viridisscale
+
+p2 = ggplot() + 
+  gg(study_area) + 
+  gg(draw2["mean"]) +
+  viridisscale
+
+p3 = ggplot() + 
+  gg(study_area) + 
+  gg(draw3["mean"]) +
+  viridisscale
+
+png(filename = "../figures/intensity_realized.png",
+    width = 15, height = 6, units = "in", res = 100)
+multiplot(p1,p2,p3, cols = 3) 
+dev.off()
 
 # GMRF params
 spde.range = spde.posterior(fit, "grf", what = "range")
 spde.logvar = spde.posterior(fit, "grf", what = "log.variance")
 
 plot(spde.range)
-ggsave(filename = "../figures/range_posterior.png")
+ggsave(filename = "../figures/range_posterior.png",
+       width = 5, height = 5, units = "in")
 
 plot(spde.logvar)
-ggsave(filename = "../figures/spde_logvar_posterior.png")
+ggsave(filename = "../figures/spde_logvar_posterior.png",
+       width = 5, height = 5, units = "in")
 
 # Abundance posterior
 source("AS.Nposterior.R") 
 predpts = ipoints(study_area, mesh)
-abnc = AS.Nposterior(fit, predpts, n.samples = 8000)   # n.samples = 1000 default
+abnc = AS.Nposterior(fit, predpts, n.sd = 5, n.grid = 250, n.samples = 20000)   # n.samples = 1000 default
 
-# Expected Abundance
+# Expected Abundance posterior
 abnc$Nexp
 
-# Abundance posterior
-plot(abnc$Npost)   # assymmetrical about mean?
+# Realized Abundance posterior
+# plot(abnc$Npost)   # assymmetrical about mean?
 
-ggplot() + 
-  gg(abnc$Npost) +
-  ggtitle(paste("Expected Abundance:  ", abnc$Nexp$mean, "\n SD: ", abnc$Nexp$sd))
+# ggplot() + 
+#   gg(abnc$Npost) +
+#   ggtitle(paste("Expected Abundance:  ", abnc$Nexp$mean, "\n SD: ", abnc$Nexp$sd))
 
-ggsave("../figures/abundance_posterior.png")
+Npost = abnc$Npost
+
+ggplot() +
+  geom_line(data = Npost, aes(x = N, y = mean)) +
+  geom_vline(aes(xintercept = abnc$Nexp$mean), colour = "blue") +
+  ggtitle("Posterior of realized abundance")
+
+ggsave("../figures/realized_abundance_posterior.png",
+       width = 5, height = 5, units = "in")
+
+Npost$Nexp = dpois(Npost$N, lambda = abnc$Nexp$mean)
+ggplot() +
+  geom_line(data = Npost, aes(x = N, y = mean)) +
+  geom_line(data = Npost, aes(x = N, y = Nexp), linetype = "dashed")
+
+ggsave("../figures/realized_abundance_vs_exp.png",
+       width = 5, height = 5, units = "in")
--- analyses/excursions.R
+++ analyses/excursions.R
@@ -3,6 +3,7 @@ library(INLA)
 library(inlabru)
 library(excursions)
 
+set.seed(1525)
 
 #### Fitted model  ####
 file_name = "akepa_2002_fitted_model.RDS"
@@ -19,7 +20,7 @@ mesh = readRDS("data/mesh_no_crs.RDS")
 # There must be a way to get the actual values from
 # predict with n.samples > 1 ?
 
-n.mc = 500
+n.mc = 500     # lower this when fiddling with things
 pxl = pixels(mesh, nx = 100, ny = 100, mask = study_area)
 X = matrix(NA, nrow = nrow(pxl@coords), ncol = n.mc)
 for (i in 1:n.mc){
@@ -37,7 +38,7 @@ Xkm = X*1000000
 Xhec = X*10000
 
 # per hectare threshold:
-u = 0.5
+u = 1
 alpha = 0.05    # Prob(intensity > u) >= 1 - alpha for all s in E
 
 ex = excursions.mc(Xhec, u = u, type = ">", alpha = alpha)
@@ -45,19 +46,36 @@ ex = excursions.mc(Xhec, u = u, type = ">", alpha = alpha)
 # plot
 Fpxl = pxl
 Fpxl$F = ex$F
+
+# png(filename = "../figures/excursion_function.png",
+#     width = 4.5, height = 5.5, units = "cm", res = 1000)
 ggplot() + 
   gg(Fpxl) +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c() +
+  ggtitle("Excursion function for > 1 bird per hectare")
+
+ggsave(filename = "../figures/excursion_function.png",
+       width = 5, height = 6, units = "in")
 
 Epxl = pxl
 Epxl$E = ex$E
 ggplot() +
-  gg(Epxl)
+  gg(Epxl) +
+  ggtitle("Excursion set for > 1 bird per hectare,\nalpha = 0.05")
+
+ggsave(filename = "../figures/excursion_set.png",
+       width = 5, height = 6, units = "in")
 
 # contour map - how to visualise this?  ?
 cm = contourmap.mc(Xhec, levels = c(0.1, 0.5, 0.8, 1.5, 2, 4), alpha = alpha)
 str(cm)
 
+Gpxl = pxl
+Gpxl$G = cm$G
+ggplot() +
+  gg(Gpxl)
+
+cm = contourmap.mc(Xhec, levels = c(1), alpha = alpha)
 Gpxl = pxl
 Gpxl$G = cm$G
 ggplot() +

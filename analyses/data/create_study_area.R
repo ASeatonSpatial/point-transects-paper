41bdb860f6aea28e738c2a8f86293551f5480ed0
A Seaton <andy.e.seaton@gmail.com>
2019-09-03 09:27:32 +0100

    create bnd_forest2.csv boundary and mesh

--- analyses/data/create_study_area.R
+++ analyses/data/create_study_area.R
@@ -7,18 +7,21 @@ library(inlabru)
 
 samplers <- readRDS("samplers_extended_no_crs.RDS")
 samplersnew = samplers[samplers$Stratum == "CF",]
-old_study_area = readRDS("study_area_no_crs.RDS")
-chull = gConvexHull(samplersnew)
-chull = gBuffer(chull, width = 500, capStyle = "SQUARE")
+# old_study_area = readRDS("study_area_no_crs.RDS")
+# chull = gConvexHull(samplersnew)
+# chull = gBuffer(chull, width = 500, capStyle = "SQUARE")
+# 
+# ggplot() +
+#   gg(old_study_area) +
+#   gg(chull) +
+#   gg(samplers) +
+#   gg(samplersnew, colour = "red") +
+#   coord_equal()
+# 
+# study_area_new = gUnion(old_study_area, chull)
 
-ggplot() +
-  gg(old_study_area) +
-  gg(chull) +
-  gg(samplers) +
-  gg(samplersnew, colour = "red") +
-  coord_equal()
-
-study_area_new = gUnion(old_study_area, chull)
+bnd.csv = read.csv("bnd_forest2.csv")
+study_area_new <- SpatialPolygons(list(Polygons(list(Polygon(bnd.csv)), ID=1)))
 
 ggplot() +
   gg(study_area_new) +

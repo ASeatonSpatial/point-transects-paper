33e6966859adc342a0558297c45a2c5d8f3057f7
A Seaton <andy.e.seaton@gmail.com>
2020-02-20 19:56:37 +0000

    update figures

--- analyses/eval_model.R
+++ analyses/eval_model.R
@@ -1,6 +1,8 @@
 # Evaluate fitted model
 library(INLA)
 library(inlabru)
+library(scales)
+library(cowplot)
 
 # set ggplot theme
 theme_set(theme_minimal())
@@ -16,6 +18,9 @@ fit = readRDS(file_name)
 study_area = readRDS("data/study_area_extended_no_crs.RDS")
 samplers = readRDS("data/samplers_extended_no_crs.RDS")
 mesh = readRDS("data/mesh_extended_no_crs.RDS")
+
+#### ggplot theme ####
+theme_set(theme_classic())
 
 #### Specify detection functions  ####
 
@@ -43,78 +48,141 @@ W = 60   # transect radius
 distdf <- data.frame(distance = seq(1,W,length=100))
 
 hnpred <- predict(fit, distdf, ~hn(distance,lsig))
-ggplot() + 
-  gg(hnpred) +
-  ggtitle("Posterior half-normal detection function")
-
-ggsave(filename = "../figures/halfnormal.png",
-       width = 5, height = 5, units = "in")
-
-# half-normal polar coords?  Is that what this is?  
-# distdf <- data.frame(distance = seq(1,W,length=100))
-# dfun <- predict(fit, distdf, ~ dsamp(distance,lsig))
-# ggplot() + 
-#   gg(dfun)
+ghn = ggplot() + 
+        gg(hnpred) +
+        ylab("probability of detection\n") +
+        xlab("\ndistance (m)") +
+        theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16))
 
 ## Matern plots ##
 
-plot(spde.posterior(fit, "grf", what = "matern.correlation")) +
-  ylab("correlation") + xlab("distance (m)") +
-  ggtitle("Posterior Matern correlation function")
+blah = spde.posterior(fit, "grf", what = "matern.correlation")
+blah$x = blah$x / 1000
+gm = ggplot() +
+      gg(blah) +
+      ylab("correlation\n") + 
+      xlab("\ndistance (km)") +
+      theme(axis.title = element_text(size = 16, vjust = 0.3),
+            axis.text = element_text(size = 16))
+      
+gm
 
-ggsave(filename = "../figures/matern_posterior.png",
-       width = 5, height = 5, units = "in")
+png(filename = "../figures/detfn_and_matern.png",
+    width = 10, height = 5, units = "in", res = 100)
+plot_grid(NULL, ghn, NULL, gm, NULL,
+          labels = c("", "A", "", "B", ""),
+          rel_widths = c(0.05, 1, 0.15, 1, 0.05),
+          ncol = 5)
+dev.off()
+
+# ggsave(filename = "../figures/matern_posterior.png",
+#        width = 5, height = 5, units = "in")
+
 
 # plot 
-ggplot() + 
-  gg(study_area) +
-  gg(mesh) +
-  gg(samplers, colour = "red") +
-  scale_fill_viridis_c() +
-  xlab("Easting") +
-  ylab("Northing") +
-  coord_equal()
+# ggplot() + 
+#   gg(study_area) +
+#   gg(mesh) +
+#   gg(samplers, colour = "red") +
+#   scale_fill_viridis_c() +
+#   xlab("Easting") +
+#   ylab("Northing") +
+#   coord_equal()
+# 
+# plot(mesh, lwd = 0.2, asp =1, main = "")
 
-plot(mesh, lwd = 0.2, asp =1, main = "")
-
-# intensity
+# intensity plots 
 pxl = pixels(mesh, nx = 100, ny = 100, mask = study_area)
 pr.int <- predict(fit, pxl, ~ exp(grf + Intercept))
 
+# mean
+lower = min(pr.int["mean"]$mean)
+lower = signif(lower, digits = 3)
+lower
+
+upper = max(pr.int["mean"]$mean)
+upper = signif(upper, digits = 3)
+upper
+
+mean_breaks = c(lower, upper)
 p1 = ggplot() + 
   gg(study_area) +
   gg(pr.int["mean"]) +
-  scale_fill_viridis_c() +
-  ggtitle("Posterior mean intensity") +  
+  scale_fill_viridis_c(breaks = mean_breaks, labels = mean_breaks) +
+  # ggtitle("A") +  
   xlab("Easting") +
   ylab("Northing") +
-  coord_equal()
+  coord_equal() +   
+  theme_void() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") +
+  guides(fill = guide_colourbar(title.vjust = 0.95,
+                                title.theme = element_text(size = 18),
+                                label.theme = element_text(size = 16)))
+
+p1 
+
+# cv
+lower = min(pr.int$cv)
+lower = signif(lower, digits = 3)
+lower
+
+upper = max(pr.int$cv)
+upper = signif(upper, digits = 3)
+upper
+
+cv_breaks = c(lower, upper)
 
 p2 = ggplot() +
   gg(study_area)  +
   gg(pr.int["cv"]) +
-  # gg(samplers, colour = "red") +
-  scale_fill_viridis_c() +
-  ggtitle("Posterior CV of intensity") +  
+  scale_fill_viridis_c(breaks = cv_breaks, labels = cv_breaks) +
+  # ggtitle("B") +  
   xlab("Easting") +
   ylab("Northing") +
-  coord_equal()
+  coord_equal() +
+  theme_void() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") +
+  guides(fill = guide_colourbar(title.vjust = 0.95,
+                                title.theme = element_text(size = 18),
+                                label.theme = element_text(size = 16)))
+
+p2 
+
+# sd
+lower = min(pr.int$sd)
+lower = signif(lower, digits = 3)
+lower = 2.23e-06
+
+upper = max(pr.int$sd)
+upper = signif(upper, digits = 3)
+upper
+
+sd_breaks = c(lower, upper)
+sd_labels = format(sd_breaks, scientific = TRUE)
 
 p3 = ggplot() +
   gg(study_area)  +
   gg(pr.int["sd"]) +
-  # gg(samplers, colour = "red") +
-  scale_fill_viridis_c() +
-  ggtitle("Posterior SD of intensity") +  
+  scale_fill_viridis_c(breaks = sd_breaks, labels = sd_labels) +
+  # ggtitle("C") +
   xlab("Easting") +
   ylab("Northing") +
-  coord_equal()
+  coord_equal() +
+  theme_void() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") +
+  guides(fill = guide_colourbar(title.vjust = 0.95,
+                                title.theme = element_text(size = 18),
+                                label.theme = element_text(size = 16)))
 
-layout_matrix <- matrix(c(4, 1, 1, 4, 2, 2, 3, 3), nrow = 2, byrow = TRUE)  # triangle arrangement
+p3
 
 png(filename = "../figures/intensity_mean_cv_sd.png",
-    width = 10, height = 10, units = "in", res = 100)
-multiplot(p1, p2, p3, layout = layout_matrix, byrow = TRUE, ncol = 2)
+    width = 10, height = 5, units = "in", res = 100)
+plot_grid(NULL, p1, NULL, p2, NULL, p3, NULL,
+          labels = c("", "A", "", "B", "", "C", ""),
+          rel_widths = c(0.1, 1, 0.4, 1, 0.4, 1, 0.1),
+          ncol = 7)
 dev.off()
 
 # png(filename = "../figures/intensity_mean_cv.png",
@@ -134,44 +202,61 @@ dev.off()
 
 # lower and upper quantiles
 
-v = c(pr.int["mean"]@data,
-      pr.int["q0.025"]@data,
-      pr.int["q0.975"]@data)
+v = c(pr.int$q0.025, pr.int$q0.975)
 
-viridisscale =   scale_fill_viridis_c(limits = range(v))
+# scale legend breaks
+lower = min(v)
+lower = signif(lower, digits = 3)
+lower = 8.32e-08
 
-p1 = ggplot() + 
-  gg(study_area) + 
-  gg(pr.int) +
-  viridisscale +
-  coord_equal() +
-  xlab("Easting") +
-  ylab("Northing")
+upper = max(v)
+upper = signif(upper, digits = 3)
+upper = 0.000943
+
+q_breaks = c(lower, upper)
+q_labels = format(q_breaks, scientific = TRUE)
 
 p2 = ggplot() +
   gg(study_area) +
   gg(pr.int["q0.025"]) +
-  viridisscale +
+  scale_fill_viridis_c(limits = range(v), breaks = q_breaks, labels = q_labels)+
   coord_equal() +
   xlab("Easting") +
-  ylab("Northing")
+  ylab("Northing") +
+  theme_void() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") +
+  guides(fill = guide_colourbar(title = "",
+                                title.vjust = 0.95,
+                                title.theme = element_text(size = 18),
+                                label.theme = element_text(size = 16)))
+
+p2
 
 p3 = ggplot() +
   gg(study_area) +
   gg(pr.int["q0.975"]) +
-  viridisscale +
+  scale_fill_viridis_c(limits = range(v), breaks = q_breaks, labels = q_labels)+
   coord_equal() +
   xlab("Easting") +
-  ylab("Northing")
+  ylab("Northing") +
+  theme_void() +
+  theme(legend.position = "bottom", legend.direction = "horizontal") +
+  guides(fill = guide_colourbar(title = "",
+                                title.vjust = 0.95,
+                                title.theme = element_text(size = 18),
+                                label.theme = element_text(size = 16)))
 
-layout_matrix <- matrix(c(4, 1, 1, 4, 2, 2, 3, 3), nrow = 2, byrow = TRUE)  # triangle arrangement
+p3
 
 png(filename = "../figures/intensity_quantiles.png",
-    width = 10, height = 10, units = "in", res = 100)
-multiplot(p1, p2, p3, layout = layout_matrix, byrow = TRUE, ncol = 2)
+    width = 10, height = 5, units = "in", res = 100)
+plot_grid(NULL, p2, NULL, p3, NULL,
+          labels = c("", "A", "", "B", ""),
+          rel_widths = c(0.05, 1, 0.15, 1, 0.05),
+          ncol = 5)
 dev.off()
 
-s# show that these maps are misleading:
+# show that these maps are misleading:
 str(pr.int)
 cell_area = as.numeric(pr.int@grid@cellsize["x"]*pr.int@grid@cellsize["y"])
 sum(pr.int@data["mean"]*cell_area)
@@ -187,32 +272,42 @@ draw_all = c(draw1["mean"]@data,
       draw2["mean"]@data,
       draw3["mean"]@data)
 
-viridisscale =   scale_fill_viridis_c(limits = range(draw_all))
+viridisscale = scale_fill_viridis_c(limits = range(draw_all))
 
 p1 = ggplot() + 
   gg(study_area) + 
   gg(draw1["mean"]) +
   viridisscale +
   xlab("Easting") +
-  ylab("Northing")
+  ylab("Northing") +
+  theme_void() +
+  theme(legend.position = "none")
 
 p2 = ggplot() + 
   gg(study_area) + 
   gg(draw2["mean"]) +
   viridisscale +
   xlab("Easting") +
-  ylab("Northing")
+  ylab("Northing") +
+  theme_void() +
+  theme(legend.position = "none")
 
 p3 = ggplot() + 
   gg(study_area) + 
   gg(draw3["mean"]) +
   viridisscale +
   xlab("Easting") +
-  ylab("Northing")
+  ylab("Northing") +
+  theme_void() +
+  theme(legend.position = "none")
 
 png(filename = "../figures/intensity_realized.png",
-    width = 15, height = 6, units = "in", res = 100)
-multiplot(p1,p2,p3, cols = 3) 
+    width = 10, height = 5, units = "in", res = 100)
+plot_grid(NULL, p1, NULL, p2, NULL, p3, NULL,
+          labels = c("", "A", "", "B", "", "C", ""),
+          rel_widths = c(0.1, 1, 0.4, 1, 0.4, 1, 0.1),
+          ncol = 7)
+
 dev.off()
 
 # GMRF params
@@ -227,28 +322,49 @@ plot(spde.logvar)
 ggsave(filename = "../figures/spde_logvar_posterior.png",
        width = 5, height = 5, units = "in")
 
-# Abundance posterior
+# Posterior abundance
 source("AS.Nposterior.R") 
 predpts = ipoints(study_area, mesh)
-abnc = AS.Nposterior(fit, predpts, n.sd = 5, n.grid = 250, n.samples = 20000)   # n.samples = 1000 default
+# abnc = AS.Nposterior(fit, predpts, 
+#                      n.sd = 5, 
+#                      n.grid = 100, 
+#                      n.samples = 100)   # n.samples = 1000 default
 
-# Expected Abundance posterior
-abnc$Nexp
+# First get range using mean + sd in expected abundance
+n.sd = 5
+n.grid = 100
+Lambda <- predict(fit, predpts, ~ sum(weight * exp(grf + Intercept))) 
+Nlow <- round(Lambda$mean - n.sd*Lambda$sd)
+Nhigh <- round(Lambda$mean + n.sd*Lambda$sd)
+by <- round((Nhigh - Nlow)/n.grid)   # should give roughly n.grid 
+Nrange <- seq(Nlow, Nhigh, by = by)
 
-# Realized Abundance posterior
-# plot(abnc$Npost)   # assymmetrical about mean?
+getNpost = function(){
+  predict(fit, predpts, 
+                ~ data.frame(N = Nrange, 
+                             dpois(Nrange, lambda = sum(weight * exp(grf + Intercept)))),
+                n.samples = 100)
+}
 
-# ggplot() + 
-#   gg(abnc$Npost) +
-#   ggtitle(paste("Expected Abundance:  ", abnc$Nexp$mean, "\n SD: ", abnc$Nexp$sd))
+n.blah = 100
+Npost = matrix(NA, nrow = n.grid+1, ncol = n.blah)
+for (i in 1:n.blah) {
+  print(paste("i = ", i))
+  Npost[,i] = getNpost()$mean
+  gc()
+}
 
-Npost = abnc$Npost
+Npost.mean = data.frame(N=Nrange, mean = rowMeans(Npost))
+saveRDS(Npost.mean, file = "Npost.mean.RDS")
 
 ggplot() +
-  geom_line(data = Npost, aes(x = N, y = mean)) +
-  geom_vline(aes(xintercept = abnc$Nexp$mean), colour = "blue") +
-  ggtitle("Posterior of realized abundance")
-
+  geom_line(data = Npost.mean, aes(x = N, y = mean)) +
+  ylab("Posterior probability\n") +
+  xlab("\nAbundance") +
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        plot.margin = margin(0.2,0.5,0.2,0.2, "in"))
+  
 ggsave("../figures/realized_abundance_posterior.png",
        width = 5, height = 5, units = "in")
 
--- analyses/excursions.R
+++ analyses/excursions.R
@@ -30,6 +30,7 @@ n.mc = 500     # lower this when fiddling with things
 pxl = pixels(mesh, nx = 100, ny = 100, mask = study_area)
 X = matrix(NA, nrow = nrow(pxl@coords), ncol = n.mc)
 for (i in 1:n.mc){
+  print(paste("i = ", i))
   int_draw <- predict(fit, pxl, ~ exp(grf + Intercept), n.samples = 1)
   X[,i] = int_draw@data$mean
 }
@@ -56,24 +57,43 @@ Fpxl$F = ex$F
 # png(filename = "../figures/excursion_function.png",
 #     width = 4.5, height = 5.5, units = "cm", res = 1000)
 p2 = ggplot() + 
+  gg(study_area) +
   gg(Fpxl) +
-  scale_fill_viridis_c() +
-  ggtitle("Excursion function for > 1 bird per hectare") +
-  xlab("Easting") +
-  ylab("Northing")
+  scale_fill_viridis_c(limits = c(0,1), 
+                       breaks = c(0, 0.5, 1),
+                       labels = c(0, 0.5, 1))+
+  coord_equal() +
+  theme_void() +
+  theme(legend.position = "bottom", 
+        legend.direction = "horizontal") +
+        #plot.margin = margin(0.1, 0, 0.1, 0, "in")) +
+  guides(fill = guide_colourbar(title.vjust = 0.95,
+                                title.theme = element_blank(),
+                                label.theme = element_text(size = 16, hjust = 0.5)))
 
 p2
+
 # ggsave(filename = "../figures/excursion_function.png",
 #        width = 5, height = 6, units = "in")
 
 Epxl = pxl
 Epxl$E = as.factor(ex$E)
+Epxl = Epxl[Epxl$E == 1,]
 p1 = ggplot() +
+  gg(study_area) +
   gg(Epxl, aes(group = E)) +
-  ggtitle("Excursion set for > 1 bird per hectare,\nalpha = 0.05") +
-  scale_fill_manual(values = c("gray", "blue")) +
-  xlab("Easting") +
-  ylab("Northing")
+  coord_equal() +
+  # ggtitle("Excursion set for > 1 bird per hectare,\nalpha = 0.05") +
+  scale_fill_manual(values = c("blue"),
+                    labels = c("Excursion set")) +
+  theme_void() +
+  theme(legend.position = "bottom", 
+        legend.direction = "vertical",
+        plot.margin = margin(0.1, 0, 0.1, 0, "in"),
+        legend.box.margin = margin(-10,0,0,0)) +
+  guides(fill = guide_legend(title.vjust = 0.95,
+                             title.theme = element_blank(),
+                             label.theme = element_text(size = 16, hjust = 0.5)))
 
 p1
 
@@ -81,8 +101,11 @@ p1
 #        width = 5, height = 6, units = "in")
 
 png(filename = "../figures/excursions.png",
-    width = 10, height = 6, units = "in", res = 100)
-multiplot(p1, p2, cols = 2)
+    width = 10, height = 5, units = "in", res = 100)
+plot_grid(NULL, p1, NULL, p2, NULL,
+          labels = c("", "A", "", "B", ""),
+          rel_widths = c(0.05, 1, 0.15, 1, 0.05),
+          ncol = 5)
 dev.off()
 
 # contour map - how to visualise this?  ?

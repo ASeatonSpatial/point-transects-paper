f0ae82c2e03f3484edde1b73b85e44d4b8326c54
A Seaton <andy.e.seaton@gmail.com>
2020-11-24 16:27:55 +0000

    model with two binary variables on det fn

--- analyses/fit_hn_covariate.R
+++ analyses/fit_hn_covariate.R
@@ -15,8 +15,7 @@ mesh <- readRDS("data/mesh_extended_no_crs.RDS")
 
 df = realobs@data
 df$HabCode = as.factor(df$HabCode)
-df$Stratum
-
+df$Stratum = as.factor(df$Stratum)
 
 ### Plot detections by Stratum
 df %>% 
@@ -31,9 +30,12 @@ df %>%
 # These look very slightly different.  Could try fitting a different detection
 # function to each
 
-# create 0-1 version of stratum factor
+# create binary variables
 realobs$stratum = ifelse(realobs$Stratum == "CF", 0, 1)
 samplers$stratum = ifelse(samplers$Stratum == "CF", 0, 1)
+
+realobs$habcode = ifelse(realobs$HabCode == "Mesic", 1, 0)
+samplers$habcode = ifelse(samplers$HabCode == "Mesic", 1, 0)
 
 #### Specify detection function  ####
 
@@ -57,7 +59,12 @@ dsamp = function(distance, x, lsig_int, lsig_x){
   log(2*pi) + log(distance) + log_hn(distance, x, lsig_int, lsig_x)
 }
 
-#### Specify and fit model ####
+
+# define distance domain and starting values 
+# for detection scale param intercept
+W <- 58/1000   # transect radius
+distance_domain <- inla.mesh.1d(seq(.Machine$double.eps, W, length.out = 30))
+starting_values <- list(lsig_int = 3.36 - log(1000))    # lsig_x = 0 implied here
 
 # SPDE:
 matern <- inla.spde2.pcmatern(mesh, 
@@ -73,9 +80,6 @@ fml <- coordinates + distance ~ grf +
   dsamp(distance, stratum, lsig_int, lsig_x) + 
   Intercept
 
-W <- 58/1000   # transect radius
-distance_domain <- inla.mesh.1d(seq(.Machine$double.eps, W, length.out = 30))
-starting_values <- list(lsig_int = 3.36 - log(1000))    # lsig_x = 0 implied here
 
 fit <-  lgcp(components = cmp, 
              data = realobs,
@@ -103,10 +107,6 @@ df %>%
   ggplot() +
   geom_histogram(aes(x = distance, y  = after_stat(density)), bins = 15) +
   facet_wrap(~ HabCode)
-
-# create binary variable
-realobs$habcode = ifelse(realobs$HabCode == "Mesic", 1, 0)
-samplers$habcode = ifelse(samplers$HabCode == "Mesic", 1, 0)
 
 # Predictor formula:
 fml <- coordinates + distance ~ grf +
@@ -154,3 +154,48 @@ ggplot() +
 # Is it surprising that mesic with 37 obs seems to be more 
 # precise than wet which has many more?
   
+
+#### Try both at once:
+
+# x1 and x2 are binary variables (2 level factor)
+# lsig_x1 adjusts for when x1 == 1
+# lsig_x2 adjusts for when x2 == 1
+# lsig_int is when x1 == 0 & x2 == 0
+
+# Log half-normal
+log_hn = function(distance, x1, x2, lsig_int, lsig_x1, lsig_x2){ 
+  -0.5*(distance/exp(lsig_int + lsig_x1*x1 + lsig_x2*x2))^2
+}
+
+# Half-normal
+hn <- function(distance, x1, x2, lsig_int, lsig_x1, lsig_x2) {
+  exp(log_hn(distance, x1, x2, lsig_int, lsig_x1, lsig_x2))
+}
+
+# Include r for switching to polar coords
+# 2*pi for not knowing angle theta
+dsamp = function(distance, x1, x2, lsig_int, lsig_x1, lsig_x2){ 
+  log(2*pi) + log(distance) + log_hn(distance, x1, x2, lsig_int, lsig_x1, lsig_x2)
+}
+
+cmp <- ~ grf(main = coordinates, model = matern) + 
+  lsig_int(1) + lsig_x1(1) + lsig_x2(1) + Intercept(1)
+
+# lsig_x1 is for habcode
+# lsig_x2 is for stratum
+fml <- coordinates + distance ~ grf +
+  dsamp(distance, habcode, stratum, lsig_int, lsig_x1, lsig_x2) +
+  Intercept
+
+fit3 <-  lgcp(components = cmp, 
+              data = realobs,
+              samplers = samplers,
+              domain = list(coordinates = mesh,
+                            distance = distance_domain),
+              formula = fml,
+              options = list(bru_max_iter = 40,
+                             bru_result = starting_values,
+                             # bru_method = list(taylor = "legacy"),
+                             bru_verbose = 3))
+
+INLA:::summary.inla(fit3)

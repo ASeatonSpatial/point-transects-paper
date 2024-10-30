d58a9d20d154dab99a8665621b86af1f02f5990b
Andy Seaton <Andy.e.seaton@gmail.com>
2024-10-30 00:05:11 +0000

    update study area to sf

--- R/data_scripts/create_study_area.R
+++ R/data_scripts/create_study_area.R
@@ -1,36 +1,26 @@
-# create polygon defining study area
-# use simple convex hull and combindation with open forest polygon for now
+# create polygon object defining study area
 
-library(sp)
-library(rgeos)
-library(inlabru)
+library(sf)
+library(ggplot2)
 
-data_path = here::here("analyses", "data")
-samplers <- readRDS(here::here(data_path, "samplers_extended_no_crs.RDS"))
-samplersnew = samplers[samplers$Stratum == "CF",]
-# old_study_area = readRDS("study_area_no_crs.RDS")
-# chull = gConvexHull(samplersnew)
-# chull = gBuffer(chull, width = 500, capStyle = "SQUARE")
-#
-# ggplot() +
-#   gg(old_study_area) +
-#   gg(chull) +
-#   gg(samplers) +
-#   gg(samplersnew, colour = "red") +
-#   coord_equal()
-#
-# study_area_new = gUnion(old_study_area, chull)
+data_raw_path = here::here("R", "data_raw")
+data_path = here::here("R", "data")
+samplers <- readRDS(here::here(data_path, "samplers.RDS"))
 
-# bnd.csv = read.csv("bnd_forest2.csv")
-bnd.csv = read.csv(here::here(data_path, "OpenClosedForestBoundary.csv"))
+bnd.csv = read.csv(here::here(data_raw_path, "OpenClosedForestBoundary.csv"))
 bnd.xy = data.frame(x = bnd.csv$EASTING / 1000,
                     y = bnd.csv$NORTHING / 1000)
-study_area_new <- SpatialPolygons(list(Polygons(list(Polygon(bnd.xy)), ID=1)))
 
+# convert to sf polygon
+bnd.poly <- st_polygon(list(as.matrix(bnd.xy)))
+bnd.sfc <- st_sfc(bnd.poly)
+bnd.sf <- st_sf(geometry = bnd.sfc)
+
+# plot to check
 ggplot() +
-  gg(study_area_new) +
-  gg(samplers) +
-  gg(samplersnew, colour = "red") +
-  coord_equal()
+  geom_sf(data = bnd.sf, alpha = 0.2) +
+  geom_sf(data = samplers)
 
-saveRDS(study_area_new, file = here::here(data_path, "study_area_extended_no_crs.RDS"))
+saveRDS(bnd.sf,
+        file = here::here(data_path,
+                          "study_area.RDS"))

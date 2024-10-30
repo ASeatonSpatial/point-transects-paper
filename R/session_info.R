8d334c20be114e6cd0119221c46432cbbf2e16d3
Andy Seaton <Andy.e.seaton@gmail.com>
2024-10-30 03:05:50 +0000

    re-run session_info.R

--- R/session_info.R
+++ R/session_info.R
@@ -1,15 +1,15 @@
 # session info for results in paper
+
 library(ggplot2)
 library(sp)
 library(inlabru)
-library(INLA)  
+library(INLA)
 library(scales)
 library(cowplot)
-library(rgeos)
 library(sf)
 library(patchwork)
 library(dplyr)
 library(excursions)
 
 writeLines(capture.output(devtools::session_info()),
-           here::here("analyses", "session_info.txt"))
+           here::here("R", "session_info.txt"))
